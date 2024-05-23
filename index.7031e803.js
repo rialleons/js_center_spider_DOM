c20d34a464dbc4daf23280804ab21646e81b55d5
Leonid Krainik <rialleons@gmail.com>
2024-05-23 11:09:43 +0300

    make build

---
+++ index.7031e803.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.top="".concat(t.clientHeight/2-e.clientHeight/2,"px"),e.style.left="".concat(t.clientWidth/2-e.clientWidth/2,"px");
+//# sourceMappingURL=index.7031e803.js.map
